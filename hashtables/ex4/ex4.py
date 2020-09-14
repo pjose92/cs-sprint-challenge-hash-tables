f2f573c9a14dc68c966c714bed0398a112b34550
Jose <pjose14.jp38@yahoo.com>
2020-09-13 20:32:43 -0500

    ex4.py completed. test passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,24 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    TODO: "need to find what positive numbers have negative corresponding numbers"
+    "results should come back with positive numbers"
+    
+    index = {}
+    result = []
+    
+    for num in a:
+        if num >= 0:
+            if num in index:
+                result.append(num)
+            else:
+                index[num] = 1
+        else:
+            negative = -num
+            if negative in index:
+                result.append(negative)
+            else:
+                index[negative] = 1
 
     return result
 
