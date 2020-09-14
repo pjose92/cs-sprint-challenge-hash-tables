aae574f7242aab3828a0941299012dade5a71dc7
Jose <pjose14.jp38@yahoo.com>
2020-09-13 21:41:57 -0500

    ex5.py completed. tests passing

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,19 @@
 # Your code here
-
-
+import os
 
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    #need to loop files base name to see if there is a match 
+    index = {}
+    result = []
+    for query in queries:
+        index[query] = 1
+    for file in files:
+        if os.path.basename(file) in index:
+            result.append(file)
 
     return result
 
